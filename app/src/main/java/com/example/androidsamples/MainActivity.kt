d68f65b63332aec32fbe9a14544370bf9b92c872
candemirci <icrimednac95@gmail.com>
2022-04-26 15:34:24 +0300

    Created Menu for Toolbar

--- app/src/main/java/com/example/androidsamples/MainActivity.kt
+++ app/src/main/java/com/example/androidsamples/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.androidsamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.ic_settings -> Toast.makeText(this, "Selected Settings", Toast.LENGTH_SHORT).show()
+            R.id.ic_add_person -> Toast.makeText(this, "Selected Add Person", Toast.LENGTH_SHORT).show()
+            R.id.ic_favorite -> Toast.makeText(this, "Selected Favorite" , Toast.LENGTH_SHORT).show()
+            R.id.ic_close -> finish()
+        }
+        return true
     }
 }
