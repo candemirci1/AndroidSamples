21d982a751e8f7bdaf4179eba1b2445c7b5eb406
candemirci <icrimednac95@gmail.com>
2022-04-27 18:37:44 +0300

    Created Fragments

--- app/src/main/java/com/example/androidsamples/MainActivity.kt
+++ app/src/main/java/com/example/androidsamples/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.androidsamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment1 = FirstFragment()
+        val fragment2 = SecondFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.framelayout, fragment1)
+            addToBackStack(null)
+            commit()
+        }
+
+        btn_first.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.framelayout, fragment1)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        btn_second.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.framelayout, fragment2)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
